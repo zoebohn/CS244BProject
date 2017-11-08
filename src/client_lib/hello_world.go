62a88f68bb4f787e8293059fab56cfbee5e7a9ff
Emma Dauterman <evd2014@gmail.com>
2017-11-08 14:28:46 -0800

    Seem to have a networked raft cluster running (giving warnings about last log entry being 0 but idk if that's an error)

--- src/client_lib/hello_world.go
+++ src/client_lib/hello_world.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("Hello world")
-	conn, err := net.Dial("tcp", ":8000")
+	conn, err := net.Dial("tcp", "127.0.0.1:50463")
 	if err != nil {
 		fmt.Println("uh oh...")
 	}
--- src/server_lib/simple_server.go
+++ src/server_lib/simple_server.go
@@ -9,8 +9,8 @@ import(
 	"log"
 	"sync"
 	"os"
-	"net"
 	"strconv"
+	"net"
 )
 
 type simpleFSM struct{
@@ -41,15 +41,137 @@ func (s *simpleSnapshot) Release() {
 
 func main() {
 
-	c := makeCluster(3, false, raft.DefaultConfig())
-	defer c.Close()
+	//c := makeCluster(3, false, raft.DefaultConfig())
+	//defer c.Close()
 
+	CreateNetworkedCluster()
 	for {}
 	// Start a raft instance(s) using simple FSM (newRaftInstance??)
 	// run it on port 8000 and have client connect
 	// try to print hello world message.	
 }
 
+func CreateNetworkedCluster() {
+	conf := raft.DefaultConfig()
+	conf.LocalID = raft.ServerID("first")
+	conf.HeartbeatTimeout = 50 * time.Millisecond
+	conf.ElectionTimeout = 50 * time.Millisecond
+	conf.LeaderLeaseTimeout = 50 * time.Millisecond
+	conf.CommitTimeout = 5 * time.Millisecond
+	conf.SnapshotThreshold = 100
+	conf.TrailingLogs = 10
+
+	// Create a single node
+	env1 := MakeRaft(conf, true)
+	WaitFor(env1, raft.Leader)
+	
+	// Join a few nodes!
+	var envs []*RaftEnv
+	for i := 0; i < 4; i++ {
+		conf.LocalID = raft.ServerID(fmt.Sprintf("next-batch-%d", i))
+		env := MakeRaft(conf, false)
+		addr := env.trans.LocalAddr()
+		env1.raft.AddVoter(conf.LocalID, addr, 0, 0)
+		envs = append(envs, env)
+	}
+}
+
+func MakeRaft(conf *raft.Config, bootstrap bool) *RaftEnv {
+	// Set the config
+	if conf == nil {
+		conf = raft.DefaultConfig()
+	}
+
+	dir, err := ioutil.TempDir("", "raft")
+	if err != nil {
+		fmt.Println("err: %v ", err)
+	}
+
+	stable := raft.NewInmemStore()
+
+	snap, err := raft.NewFileSnapshotStore(dir, 3, nil)
+	if err != nil {
+		fmt.Println("err: %v", err)
+	}
+
+	env := &RaftEnv{
+		conf:     conf,
+		dir:      dir,
+		store:    stable,
+		snapshot: snap,
+		fsm:      &simpleFSM{},
+		logger:	  log.New(os.Stdout, "", log.Lmicroseconds),
+	}
+	trans, err := raft.NewTCPTransport("127.0.0.1:0", nil, 2, time.Second, nil)
+	if err != nil {
+		fmt.Println("err: %v", err)
+	}
+	env.logger = log.New(os.Stdout, string(trans.LocalAddr())+" :", log.Lmicroseconds)
+	env.trans = trans
+
+	if bootstrap {
+		var configuration raft.Configuration
+		configuration.Servers = append(configuration.Servers, raft.Server{
+			Suffrage: raft.Voter,
+			ID:       conf.LocalID,
+			Address:  trans.LocalAddr(),
+		})
+		err = raft.BootstrapCluster(conf, stable, stable, snap, trans, configuration)
+		if err != nil {
+			fmt.Println("err: %v", err)
+		}
+	}
+	log.Printf("[INFO] Starting node at %v", trans.LocalAddr())
+	conf.Logger = env.logger
+	raft, err := raft.NewRaft(conf, env.fsm, stable, stable, snap, trans)
+	if err != nil {
+		fmt.Println("err: %v", err)
+	}
+	env.raft = raft
+	return env
+}
+
+func WaitFor(env *RaftEnv, state raft.RaftState) error {
+	limit := time.Now().Add(200 * time.Millisecond)
+	for env.raft.State() != state {
+		if time.Now().Before(limit) {
+			time.Sleep(10 * time.Millisecond)
+		} else {
+			return fmt.Errorf("failed to transition to state %v", state)
+		}
+	}
+	return nil
+}
+
+type RaftEnv struct {
+	dir      string
+	conf     *raft.Config
+	fsm      *simpleFSM
+	store    *raft.InmemStore
+	snapshot *raft.FileSnapshotStore
+	trans    *raft.NetworkTransport
+	raft     *raft.Raft
+	logger	 *log.Logger
+}
+
+// Release shuts down and cleans up any stored data, its not restartable after this
+func (r *RaftEnv) Release() {
+	r.Shutdown()
+	os.RemoveAll(r.dir)
+}
+
+// Shutdown shuts down raft & transport, but keeps track of its data, its restartable
+// after a Shutdown() by calling Start()
+func (r *RaftEnv) Shutdown() {
+	fmt.Println("[WARN] Shutdown node")
+	f := r.raft.Shutdown()
+	if err := f.Error(); err != nil {
+		panic(err)
+	}
+	r.trans.Close()
+}
+
+// NOT IN USE
 func makeCluster(n int, bootstrap bool, conf *raft.Config) *cluster {
 	if conf == nil {
 		conf = raft.DefaultConfig()
@@ -85,7 +207,7 @@ func makeCluster(n int, bootstrap bool, conf *raft.Config) *cluster {
 		c.snaps = append(c.snaps, snaps)
 
 		addr := &net.TCPAddr{IP: []byte{127,0,0,1}, Port: 8000+i}
-		trans, err := raft.NewTCPTransportWithLogger("0.0.0.0:0", addr, 1, 0, nil)
+		trans, err := raft.NewTCPTransportWithLogger("127.0.0.1:" + strconv.Itoa(8000+i), addr, 1, time.Second, nil)
 		if err != nil {
 			fmt.Println("ERROR with TCP transport")
 		}
@@ -97,13 +219,15 @@ func makeCluster(n int, bootstrap bool, conf *raft.Config) *cluster {
 		configuration.Servers = append(configuration.Servers, raft.Server{
 			Suffrage: raft.Voter,
 			ID:       localID,
-			Address:  raft.ServerAddress(strconv.Itoa(i)), // PLACEHOLDER
+			Address:  trans.LocalAddr(), //raft.ServerAddress(strconv.Itoa(i)), // PLACEHOLDER
 		})
 	}
 
 	// TODO: Peers don't know about each other, need to make sure communicating.
 	// Wire the transports together
 //	c.FullyConnect()
+
+	var raft1 *raft.Raft
 
 	// Create all the rafts
 	c.startTime = time.Now()
@@ -128,6 +252,12 @@ func makeCluster(n int, bootstrap bool, conf *raft.Config) *cluster {
 			fmt.Println("ERROR creating new raft\n")	
 		}
 
+		if i == 0 {
+			raft1 = raft
+		}
+		if i > 0 {
+			raft1.AddVoter(peerConf.LocalID, trans.LocalAddr(), 0, 0)
+		}
 		c.rafts = append(c.rafts, raft)
 	}
 	return c
