3b8a79d9b16294957233b3d22471127444ccbfda
Zoë Bohn <zbohn2@DN51ul9k.SUNet>
2017-11-29 20:41:33 -0800

    missing files

---
+++ src/locks/utils.go
@@ -0,0 +1,18 @@
+ package locks
+
+ func lock_array_to_string(lock_arr []Lock) string {
+    var string_form []string
+    for _, l := range lock_arr {
+        string_form = append(string_form, string(l))
+    }
+    return strings.Join(string_form, ";")
+ }
+
+ func string_to_lock_array(s string) []Lock {
+    string_arr := strings.Split(s, ";")
+    var lock_arr []Lock
+    for _, l := range string_arr {
+        lock_arr = append(lock_arr, Lock(l))
+    }
+    return lock_arr
+ }
